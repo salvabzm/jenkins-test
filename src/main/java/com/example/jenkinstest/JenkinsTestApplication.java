e1d1cf7f542f2a2b836bd03ef6d8013bab8c0675
Salvador Zaragoza <salva962008@hotmail.com>
2023-03-26 18:42:32 -0600

    Extend missing SpringBootServletInitializer

--- src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
@@ -2,12 +2,18 @@ package com.example.jenkinstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsTestApplication {
+public class JenkinsTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JenkinsTestApplication.class);
+    }
 }
